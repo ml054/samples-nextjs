743f1ee1fdf47bbe1df3fb85d298ee9658f4b6c8
Damian Olszewski <damian@ravendb.net>
2024-05-27 10:15:53 +0200

    Add comment to store.ts

--- src/db/store.ts
+++ src/db/store.ts
@@ -1,5 +1,6 @@
 import { DocumentStore } from "ravendb";
 
+// Change it to your url and database name
 const store = new DocumentStore("http://live-test.ravendb.net", "beer-order-database");
 store.initialize();
 
